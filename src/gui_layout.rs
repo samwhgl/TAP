d19232db92d8b84d57ab7bb54e01da730a1abd4b
kprist <kprist91@gmail.com>
2026-06-24 16:07:25 +0200

    Connection chat messages to chat windows
    Messages are now displayed in the correct window when received.
    ToDo:
    - format messages correctly.
    - add color.

--- src/gui_layout.rs
+++ src/gui_layout.rs
@@ -23,8 +23,6 @@ impl GuiWindow {
         _cc: &eframe::CreationContext<'_>,
         tx: tokio::sync::mpsc::Sender<String>,
         rx: std::sync::mpsc::Receiver<String>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
         Self {
             input: String::new(),
@@ -39,7 +37,7 @@ impl GuiWindow {
         }
     }
 
-    fn draw_left_panel(&mut self, ui: &mut egui::Ui) {
+    fn draw_left_panel(&mut self, ui: &mut egui::Ui, new_chat_msgs: [bool; 3]) {
       egui::Panel::left("left_panel")
             .default_size(400.0)
             .show_inside(ui, |ui| {
@@ -69,7 +67,13 @@ impl GuiWindow {
                                         .max_height(scroll_height)
                                         .auto_shrink([false, false])
                                         .show(ui, |ui| {
-                                            ui.small("message de test...");
+                                            for log in &self.chats_logs[i] {
+                                                ui.label(RichText::new(log.trim()).color(egui::Color32::LIGHT_GRAY));
+                                            }
+                                            
+                                            if new_chat_msgs[i] {
+                                                ui.scroll_to_cursor(Some(egui::Align::BOTTOM));
+                                            }
                                     });
                                 });
                             
@@ -87,7 +91,7 @@ impl GuiWindow {
                                 );
 
                                 let button_clicked = button_response.clicked();
-                                let enter_pressed = text_edit_response.lost_focus() && ui.input(|ins| ins.key_pressed(egui::Key::Enter));
+                                let enter_pressed = text_edit_response.lost_focus() && ui.input(|input_state| input_state.key_pressed(egui::Key::Enter));
                                 if button_clicked || enter_pressed {
                                     if !self.chats_inputs[i].is_empty() {
                                         let command = format!("{} {}\n", chat_commands[i], self.chats_inputs[i].trim());
@@ -116,25 +120,77 @@ impl GuiWindow {
             ("USE ...", ButtonAction::Fill),
             ("DROP ...", ButtonAction::Fill),
             ("ATTACK ...", ButtonAction::Fill),
-            ("CREATE GROUP ...", ButtonAction::Fill),
-            ("JOIN GROUP ...", ButtonAction::Fill),
-            ("LEAVE GROUP ...", ButtonAction::Fill),
+            ("GROUP CREATE ...", ButtonAction::Fill),
+            ("GROUP JOIN ...", ButtonAction::Fill),
+            ("GROUP LEAVE ...", ButtonAction::Fill),
         ];
         egui::Panel::right("right_panel")
             .default_size(200.0)
             .show_inside(ui, |ui| {
                 ui.allocate_ui_with_layout(
-                    egui::Vec2 { x: ui.available_width(), y: ui.available_width()},
+                    egui::Vec2::splat(ui.available_width()),
                     egui::Layout::top_down(egui::Align::Center),
                     |ui| {
                         egui::Frame::NONE
                             .fill(egui::Color32::from_gray(30))
                             .corner_radius(4.0)
                             .show(ui, |ui| {
-                                ui.set_min_size(egui::Vec2::new(ui.available_width() - 30.0, ui.available_width() - 30.0));
-                                ui.centered_and_justified(|ui| {
-                                    ui.small("Temp Text")
-                                })
+                                let square_size = egui::Vec2::splat(ui.available_width() - 10.0);
+                                let (rect, _) = ui.allocate_exact_size(square_size, egui::Sense::click());
+
+                                let painter = ui.painter_at(rect);
+                                let center = rect.center();
+                                let circle_radius = square_size.x / 2.0 - 10.0;
+                                painter.circle_filled(
+                                    center, 
+                                    circle_radius, 
+                                    egui::Color32::from_rgb(15, 15, 15)
+                                );
+                                painter.text(
+                                    center,
+                                    egui::Align2::CENTER_CENTER,
+                                    "MOVE",
+                                    egui::FontId::proportional(14.0),
+                                    egui::Color32::from_gray(180)
+                                );
+
+                                let half_base = square_size.x / 6.0;       // Size of the base of a triangle
+                                let height = square_size.x / 6.0;          // Height of the triangle button
+                                let offset = circle_radius * 0.5;           // Distance from the center of the circle
+
+                                let directions = [
+                                    ("north", [center + egui::Vec2::new(0.0, -offset - height), center + egui::Vec2::new(-half_base, -offset), center + egui::Vec2::new(half_base, -offset)]),
+                                    ("south", [center + egui::Vec2::new(0.0, offset + height), center + egui::Vec2::new(-half_base, offset), center + egui::Vec2::new(half_base, offset)]),
+                                    ("west",  [center + egui::Vec2::new(-offset - height, 0.0), center + egui::Vec2::new(-offset, -half_base), center + egui::Vec2::new(-offset, half_base)]),
+                                    ("east",  [center + egui::Vec2::new(offset + height, 0.0), center + egui::Vec2::new(offset, -half_base), center + egui::Vec2::new(offset, half_base)]),
+                                ];
+
+                                for (cmd, points) in directions {
+                                    let bounding_rect = egui::Rect::from_points(&points);
+                                    let is_hovered = ui.rect_contains_pointer(bounding_rect);
+                                    let is_clicking = is_hovered && ui.input(|i| i.pointer.any_down());
+                                    
+                                    let color = if is_clicking {
+                                        egui::Color32::LIGHT_BLUE
+                                    } else if is_hovered {
+                                        egui::Color32::LIGHT_GRAY
+                                    } else {
+                                        egui::Color32::GRAY
+                                    };
+
+                                    if is_hovered && ui.input(|i| i.pointer.any_pressed()) {
+                                        let command_to_send = format!("MOVE {}\n", cmd);
+                                        let _ = self.tx.try_send(command_to_send);
+                                        
+                                        self.display_logs.push(format!("=> MOVE {}", cmd));
+                                    }
+
+                                    painter.add(egui::Shape::convex_polygon(
+                                        points.to_vec(),
+                                        color,
+                                        egui::Stroke::NONE,
+                                    ));
+                                }
                             })
                     });
                 ui.separator();
@@ -164,7 +220,6 @@ impl GuiWindow {
     }
     fn draw_bottom_panel(&mut self, ui: &mut egui::Ui) {
         egui::Panel::bottom("bottom_panel").show_inside(ui, |ui| {
-            // The top panel is often a good place for a menu bar:
 
             ui.horizontal(|ui| {
                 let button_width = 50.0;
@@ -176,7 +231,7 @@ impl GuiWindow {
                 );
                 if self.focus_main_input {
                     command_input.request_focus();
-                    self.focus_main_input = false; // On reset le drapeau
+                    self.focus_main_input = false;
                 }
                 let button_clicked = ui.add_sized(
                         [button_width, ui.spacing().interact_size.y],
@@ -205,18 +260,26 @@ impl GuiWindow {
 impl eframe::App for GuiWindow {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         let mut new_msg = false;
+        let mut new_chat_msgs = [false; 3];
         while let Ok(msg) = self.rx.try_recv() {
-            self.display_logs.push(msg);
-            new_msg = true;
+            if msg.starts_with("EVT GLOBAL CHAT") {
+                self.chats_logs[0].push(msg);
+                new_chat_msgs[0] = true;
+            } else if msg.starts_with("EVT ROOM CHAT") {
+                self.chats_logs[1].push(msg);
+                new_chat_msgs[1] = true;
+            } else if msg.starts_with("EVT GROUP CHAT") {
+                self.chats_logs[2].push(msg);
+                new_chat_msgs[2] = true;
+            } else {
+                self.display_logs.push(msg);
+                new_msg = true;
+            }
         }
 
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-        self.draw_left_panel(ui);
+        self.draw_left_panel(ui, new_chat_msgs);
         self.draw_right_panel(ui);
         self.draw_bottom_panel(ui);
-
-        
 
 
         let central_area = ui.available_rect_before_wrap();
